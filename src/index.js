2e2b2ecd5d3342dc3c5ce5452f2a3587f57e7ca9
akinolapo <akinolapo@gamil.com>
2022-05-18 23:17:36 +0100

    change src folder

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App/>, document.getElementById('root'))
+import './index.css';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
