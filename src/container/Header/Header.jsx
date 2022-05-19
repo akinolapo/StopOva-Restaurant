03b24d156bdb72ae2a67e00081a718da082bcf89
Peace Akinola <akinolapo@gmail.com>
2022-05-19 02:23:22 +0100

    Header container

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { SubHeading } from '../../components';
 
+import {images} from '../../constants'
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className="app__wrapper_info">
+      <SubHeading title='Good Food. Good Mood'/>
+      <h1 className='app__header-h1'>Home of delicious meals</h1>
+      <p className='p__opensans' style={{margin:'2rem 0'}}>We offer the finest quality & nourishing meals, using our original recipes. Local dishes, African dishes, Small chops, snacks, Ice cream etc, are all available.</p>
+      <button type='button' className='custom__button'>Explore Menu</button>
+    </div>
+
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt='header img'></img>
+    </div>
   </div>
 );
 
