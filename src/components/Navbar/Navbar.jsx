860a01bb552d509490939abc98a563768196a0f4
Peace Akinola <akinolapo@gamil.com>
2022-05-19 01:10:23 +0100

    Change Logo and color schema

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <nav className='app__navbar'>
       <div className="app__navbar-logo">
-        <img src={images.gericht} alt='app logo'></img>
+        <img src={images.StopOva} alt='app logo'></img>
       </div>
       <ul className='app__navbar-links'>
         <li className='p__opensans'><a href='#home'>Home</a></li>
