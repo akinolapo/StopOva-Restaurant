afe77130fc07288ddbbddfb2f77dac47960310d1
Peace Akinola <akinolapo@gmail.com>
2022-05-19 22:31:12 +0100

    Started About

--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -6,7 +6,7 @@ import './AboutUs.css';
 const AboutUs = () => (
   <div className='app__aboutus app__bg flex__center section__padding' id='about'>
     <div className="app__aboutus-overlay  flex__center">
-      <img src={images.G} alt='G'></img>
+      <img src={images.S} alt='G'></img>
     </div>
 
     <div className="app__aboutus-content  flex__center">
@@ -14,7 +14,20 @@ const AboutUs = () => (
         <h1 className='headtext__cormorant'>About Us</h1>
         <img src={images.spoon} alt='about_spoon' className='spoon__img'></img>
         <p className='p__opensans'>Cras fringilla, arcu sed viverra porta, lacus erat hendrerit magna, eget tempus massa risus quis quam.</p>
+        <button type='button' className='custom__button'>Know More</button>
       </div>
+
+      <div className="app__aboutus-content_knife flex__center">
+        <img src={images.knife} alt='about_knife'></img>
+      </div>
+
+      <div className="app__aboutus-content_about">
+        <h1 className='headtext__cormorant'>About Us</h1>
+        <img src={images.spoon} alt='about_spoon' className='spoon__img'></img>
+        <p className='p__opensans'>Cras fringilla, arcu sed viverra porta, lacus erat hendrerit magna, eget tempus massa risus quis quam.</p>
+        <button type='button' className='custom__button'>Know More</button>
+      </div>
+
     </div>
 
   </div>
