167ad55fc3d9ca230d7698dcb440c69783c4432a
Peace Akinola <akinolapo@gmail.com>
2022-05-20 18:49:58 +0100

    About Done

--- src/constants/images.js
+++ src/constants/images.js
@@ -2,6 +2,7 @@ import bg from '../assets/bg.png';
 import chef from '../assets/chef.png';
 import G from '../assets/G.png';
 import S from '../assets/S.png';
+import SO from '../assets/SO.png'
 import gallery01 from '../assets/gallery01.png';
 import gallery02 from '../assets/gallery02.png';
 import gallery03 from '../assets/gallery03.png';
@@ -27,6 +28,7 @@ export default {
   chef,
   G,
   S,
+  SO,
   gallery01,
   gallery02,
   gallery03,
--- src/container/AboutUs/AboutUs.jsx
+++ src/container/AboutUs/AboutUs.jsx
@@ -6,7 +6,7 @@ import './AboutUs.css';
 const AboutUs = () => (
   <div className='app__aboutus app__bg flex__center section__padding' id='about'>
     <div className="app__aboutus-overlay  flex__center">
-      <img src={images.S} alt='G'></img>
+      <img src={images.G} alt='G'></img>
     </div>
 
     <div className="app__aboutus-content  flex__center">
@@ -21,8 +21,8 @@ const AboutUs = () => (
         <img src={images.knife} alt='about_knife'></img>
       </div>
 
-      <div className="app__aboutus-content_about">
-        <h1 className='headtext__cormorant'>About Us</h1>
+      <div className="app__aboutus-content_history">
+        <h1 className='headtext__cormorant'>Our History</h1>
         <img src={images.spoon} alt='about_spoon' className='spoon__img'></img>
         <p className='p__opensans'>Cras fringilla, arcu sed viverra porta, lacus erat hendrerit magna, eget tempus massa risus quis quam.</p>
         <button type='button' className='custom__button'>Know More</button>
