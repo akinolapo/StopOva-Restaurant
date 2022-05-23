2faf4947a6dab438e3f252763662fd8273f1baac
Peace Akinola <akinolapo@gmail.com>
2022-05-23 10:03:41 +0100

    menu completed

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -16,7 +16,7 @@ const SpecialMenu = () => (
         <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
         <div className="app__specialMenu_menu_items">
           {data.wines.map((wine, index) =>
-          <p>{wine.title}</p>          
+          <MenuItem key={wine.title + index} title={wine.title} price={wine.price} tags={wine.tags}/>          
           )}
         </div>
       </div>
@@ -24,6 +24,19 @@ const SpecialMenu = () => (
       <div className="app__specialMenu-menu_img">
         <img src={images.menu} alt='Menu img'></img>
       </div>
+
+      <div className="app__specialMenu-menu_cocktails flex__center">
+        <p className='app__specialMenu_menu_heading'>Cocktails</p>
+        <div className="app__specialMenu_menu_items">
+          {data.cocktails.map((cocktail, index) =>
+          <MenuItem key={cocktail.title + index} title={cocktail.title} price={cocktail.price} tags={cocktail.tags}/>      
+          )}
+        </div>
+      </div>
+
+    </div>
+    <div style={{marginTop: '15px'}}>
+      <button type='button' className='custom__button'>View More</button>
     </div>
   </div>
 );
